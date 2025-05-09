b710707e1ab98af7827f312bd90e6d99b0a34b2d
Fadl711 <fathlvvee@gmail.com>
2025-05-09 19:24:22 +0300

    Df

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ export const NavBar = () => {
     <nav
       className={cn(
         "fixed w-full z-40 transition-all duration-300",
-        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        isScrolled ? "py-3 bg-background/80  shadow-xs" : "py-5"
       )}
     >
       <div className="container flex items-center justify-between">
@@ -40,7 +40,7 @@ export const NavBar = () => {
         </a>
 
         {/* desktop nav */}
-        <div className="hidden md:flex space-x-8">
+        <div className="hidden md:flex space-x-8 mr-24">
           {navItems.map((item, key) => (
             <a
               key={key}
